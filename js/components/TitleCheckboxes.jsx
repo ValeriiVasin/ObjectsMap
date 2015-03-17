c1e351d9240d1b21200bc3288f711055e8a77698
Valerii Vasin <valerii.vasin@gmail.com>
2015-03-18 00:30:18 +0100

    Implement feedback from TitleCheckboxes.

--- js/components/TitleCheckboxes.jsx
+++ js/components/TitleCheckboxes.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import uid from 'lodash/utility/uniqueId'
+import assign from 'lodash/object/extend'
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      checked: {}
+    };
+  },
+
+  onChange(title, event) {
+    let fieldObj = {
+      [title]: event.target.checked
+    }
+
+    let checked = assign({}, this.state.checked, fieldObj);
+
+    this.setState({
+      checked: checked
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange(checked);
+    }
+  },
+
   render() {
     if (!this.props.titles || !this.props.titles.length) {
       return null;
@@ -11,7 +34,12 @@ export default React.createClass({
       let _uid = uid('title_uid_');
       return (
         <label key={_uid} htmlFor={_uid} className="checkbox-inline" value={title}>
-          <input type="checkbox" id={_uid} />{title}
+          <input
+            type="checkbox"
+            checked={this.state.checked[title]}
+            id={_uid}
+            onChange={this.onChange.bind(this, title)}
+            />{title}
         </label>
       );
     });
