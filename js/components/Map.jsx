3a2053cee60ceddb34834bd8b9cfb45ec9c58b27
Valerii Vasin <valerii.vasin@gmail.com>
2015-03-18 01:15:08 +0100

    Implement address type switching.

--- js/components/Map.jsx
+++ js/components/Map.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import Reflux from 'reflux';
 
-// * Select marker title
-//
-// * Select fields to find place by:
-//   types: [lat/lng, address]
-//   - lat/lng - selects with all fields
-//   - address => selects + add field
+import MapSettings from './MapSettings.jsx';
 
 export default React.createClass({
   render() {
@@ -15,32 +10,10 @@ export default React.createClass({
     }
 
     let titles = Object.keys(this.props.projects[0]);
-    let selected = titles[0];
-
-    let markerTitle = (
-      <div>
-        <h1>Map settings:</h1>
-        <label htmlFor="marker-title">Marker title:</label>
-        <select value={selected}>
-          {titles.map((title, index) => <option key={index} children={title}></option>)}
-        </select>
-      </div>
-    );
-
-    let markerPositionType = (
-      <div className="mapMarkerType">
-        <label htmlFor="marker-type-latlng">Latitude / Longitude</label>
-        <input name="markerType" type="radio" value="latlng" />
-
-        <label htmlFor="marker-type-address">Address</label>
-        <input name="markerType" type="radio" value="address" />
-      </div>
-    );
 
     return (
       <div>
-        {markerTitle}
-        {markerPositionType}
+        {<MapSettings titles={titles} />}
       </div>
     );
   }
--- js/components/MapSettings.jsx
+++ js/components/MapSettings.jsx
@@ -3,6 +3,13 @@ import uid from 'lodash/utility/uniqueId'
 
 import TitleSelector from './TitleSelector.jsx';
 
+// * Select marker title
+//
+// * Select fields to find place by:
+//   types: [lat/lng, address]
+//   - lat/lng - selects with all fields
+//   - address => selects + add field
+
 export default React.createClass({
   getInitialState() {
     return {
@@ -23,6 +30,11 @@ export default React.createClass({
     this.setState({ markerField: field });
   },
 
+  setAddressType(value) {
+    this.setState({ addressType: value })
+    console.log(value);
+  },
+
   render() {
     if (!this.props.titles || !this.props.titles.length) {
       return null;
@@ -35,10 +47,42 @@ export default React.createClass({
       </div>
     );
 
+    let uidLat = uid('label_');
+    let uidLng = uid('label_');
+    let name = uid('name_');
+
+    let addressType = (
+      <div className="form-group">
+        <h3>Select address type</h3>
+        <label htmlFor={uidLat} className="checkbox-inline">
+          <input
+            type="radio"
+            name={name}
+            id={uidLat}
+            value="latlng"
+            selected={this.state.addressType === 'latlng'}
+            onChange={this.setAddressType.bind(this, 'latlng')}
+            />Latitude / Longitude
+        </label>
+
+        <label htmlFor={uidLng} className="checkbox-inline">
+          <input
+            type="radio"
+            name={name}
+            id={uidLng}
+            value="address"
+            selected={this.state.addressType === 'address'}
+            onChange={this.setAddressType.bind(this, 'address')}
+            />Address
+        </label>
+      </div>
+    );
+
     return (
       <div>
         <h1>Map settings!</h1>
         {markerField}
+        {addressType}
       </div>
     );
   }
